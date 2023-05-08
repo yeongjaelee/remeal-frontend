e2d4f72b68ff76a5c32f4744b5330b22043780c6
yeongjae <yeongjae.blocket@gmail.com>
2023-05-08 13:50:00 +0900

    login layout

--- app/nav/NavLayout.tsx
+++ app/nav/NavLayout.tsx
@@ -13,19 +13,22 @@ export default function NavLayout({
 }) {
     const [showModal, setShowModal] = useState(false);
     const token = localStorage.getItem('token')
+    const userEmailFirst = localStorage.getItem('userEmailFirst')
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search)
         const token = urlParams.get('token')
         const refreshToken = urlParams.get('refreshToken')
-        if (typeof token === "string" && typeof refreshToken==="string") {
+        const userEmailFirst = urlParams.get('userEmailFirst')
+        if (typeof token === "string" && typeof refreshToken==="string" && typeof userEmailFirst==="string") {
             localStorage.setItem('token', token)
             localStorage.setItem('refreshToken', refreshToken)
+            localStorage.setItem('userEmailFirst', userEmailFirst)
             window.location.replace('/')
         }
     }, []);
     return (
         <div className="h-12">
-            <div className="flex justify-between h-12 pl-4 pt-4 pb-4 pr-4">
+            <div className="flex justify-between items-center h-12 pl-4 pt-4 pb-4 pr-4">
                 <div className="flex flex-row items-center">
                     <div>
                         <Link href="/" className="homepageButton">
@@ -33,17 +36,17 @@ export default function NavLayout({
                         </Link>
                     </div>
                 </div>
-                <div className="flex items-center">
+                <div className="flex flex-row items-center h-12">
                     <Link href="../post/postCreate">
                         <div className="flex flex-row items-center justify-center">
                             <FontAwesomeIcon icon={faPenToSquare} style={{color: "#8c95a6",}} />
-                            <p className="text-gray-400 ml-1 text-sm">Write</p>
+                            <p className="text-gray-400 ml-1 text-sm font-light">Write</p>
                         </div>
                     </Link>
                     {token ?
-                        <div>
-                            you are log in
-                        </div>
+                        <Link href="../profile" className="relative rounded-full w-6 bg-green-500 flex ml-3 items-center justify-center h-6">
+                            <p className="text-gray-100 flex items-center justify-center mb-0.5">{userEmailFirst}</p>
+                        </Link>
                         :
                         <button onClick={() => setShowModal(true)} className="loginButton ml-4">
                             <FontAwesomeIcon icon={faArrowRightFromBracket} className="w-4 h-4" />
@@ -56,6 +59,7 @@ export default function NavLayout({
                         Hello from the modal!
                     </LoginModal>
                 </div>
+
             </div>
             {children}
         </div>
