b5d3ba305601bf6860d34ccc40816efbf9eaedae
yeongjae <yeongjae.blocket@gmail.com>
2023-06-14 17:19:37 +0900

    temp

--- apollo-client.ts
+++ apollo-client.ts
@@ -8,7 +8,8 @@ import UserService from "./app/data/users";
 import auth_client from "./auth-client";
 import {createUploadLink} from "apollo-upload-client";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 const httpLink = new createUploadLink({
     uri: "http://127.0.0.1:8000/graphql",
 
