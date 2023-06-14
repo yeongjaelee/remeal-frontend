bd500b6eb78117ec0b670dd08d920f09357cec09
yeongjae <yeongjae.blocket@gmail.com>
2023-06-14 16:08:47 +0900

    temp

--- app/components/searchBar/page.tsx
+++ app/components/searchBar/page.tsx
@@ -1,22 +1,22 @@
-'use client';
-import {useState} from "react";
-
-export default function SearchBarPage({onSearch}) {
-    const [query, setQuery] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSearch(query);
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="type and enter plz"
-            />
-        </form>
-    );
-}
+// 'use client';
+// import {useState} from "react";
+//
+// export default function SearchBarPage({onSearch}) {
+//     const [query, setQuery] = useState("");
+//
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         onSearch(query);
+//     };
+//
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <input
+//                 type="text"
+//                 value={query}
+//                 onChange={(e) => setQuery(e.target.value)}
+//                 placeholder="type and enter plz"
+//             />
+//         </form>
+//     );
+// }
