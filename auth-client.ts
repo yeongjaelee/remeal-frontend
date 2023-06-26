55a35996b8ffc2e4f7482afe99e7513b6f3b834f
yeongjae <yeongjae.blocket@gmail.com>
2023-06-26 23:02:46 +0900

    temp

--- auth-client.ts
+++ auth-client.ts
@@ -2,7 +2,7 @@ import {ApolloClient, InMemoryCache} from "@apollo/client";
 import client from "./apollo-client";
 
 const auth_client = new ApolloClient({
-    uri: "3.38.85.232:8000/graphql",
+    uri: "http://3.38.85.232:8000/graphql",
     // uri: `${process.env.END_POINT}/graphql`,
     cache: new InMemoryCache(),
 });
