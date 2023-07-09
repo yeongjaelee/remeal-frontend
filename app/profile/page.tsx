fbcb28fa86bfca009af96440b3630587c0f8e2bc
yeongjae <yeongjae.blocket@gmail.com>
2023-07-09 20:45:32 +0900

    temp

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -246,7 +246,7 @@ export default function Page() {
                     <div className="h-16"></div>
                     {userImage && !isUserImageDeleted?
                         <div className="relative rounded-full w-20 h-20 flex ml-3 items-center justify-center ">
-                            <Image src={userImage} className="rounded-full w-full h-full object-cover" alt=""/>
+                            <img src={userImage} className="rounded-full w-full h-full object-cover" alt=""/>
                         </div>
                         :
                         <div className="relative rounded-full w-20 h-20 bg-emerald-700 flex ml-3 items-center justify-center ">
